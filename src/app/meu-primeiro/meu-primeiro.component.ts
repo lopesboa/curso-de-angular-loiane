d5ba89b1a6b054feb2dfa88e48746a6c04529258
lopesboa <lopesboa1@gmail.com>
2020-03-03 21:54:46 -0300

    Working with angular module

---
+++ src/app/meu-primeiro/meu-primeiro.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'meu-primeiro-component',
+  templateUrl: 'meu-primeiro.component.html',
+  styleUrls: ['meu-primeiro.component.scss']
+})
+
+export class MeuPrimeiroComponent {
+  constructor () {}
+}
